0ceb74af8ffb5aaa3d9b9ebdb5303ccc07da60db
Sivan wolberg <sivan@wolberg.pro>
2025-04-02 00:36:35 +0300

    refactor: remove unused page and authentication route files, update CMS package name

--- apps/backoffice/src/app/api/auth/[...all]/route.ts
+++
@@ -1,6 +0,0 @@
-import { auth } from '@repo/utils';
-import { toNextJsHandler } from 'better-auth/next-js';
-
-const handler = toNextJsHandler(auth.handler);
-
-export const { GET, POST } = handler;
